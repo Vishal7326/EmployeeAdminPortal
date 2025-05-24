325c5f3fde2d79201cd91d329273103545778fc8
Vishal7326 <vishalp65276@gmail.com>
2025-05-24 19:54:17 +0530

    Added Models Entities and Employee Classes with all the required fields in the Employee Table.

---
+++ Models/Entities/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace EmployeeAdminPortal.Models.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; } // Guid - Global Unique Identifier.
+
+        public required string Name { get; set; } //Non-nullable property.
+
+        public required string Email { get; set; }
+
+        public string? Phone { get; set; } // Nullable property, can be null using question mark to make it nullable property.
+
+        public decimal? Salary { get; set; }
+    }
+}
