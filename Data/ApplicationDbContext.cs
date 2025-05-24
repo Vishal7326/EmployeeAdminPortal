5bbf9e9c04771875a4f7b3592fedcf83824f1d0c
Vishal7326 <vishalp65276@gmail.com>
2025-05-24 21:28:55 +0530

    Created Data Folder to create dbContext Class created Generic DbContext their and Employee Property to it.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmployeeAdminPortal.Models.Entities;
+namespace EmployeeAdminPortal.Data
+    
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base (options) //DbContextOptions<ApplicationDbContext> is a Generic class that tells how to configure to the database whereas base is used to call the constructor of the parent class and options is passed is in as parameter.
+        {
+            
+        }
+
+       //Next What we are going do to the class is that we will add a property or the collection that we are going to store in the database.
+        //dbset is used to interact with the database table.
+        //We are going to store the employees in the database hence we need to create dbset of employees here
+
+        public DbSet<Employee> Employees { get; set; } //This tells Entity Framework “I want to create and manage a table of Employee objects in the database.”
+        //The Employee class represents the structure(columns) of each row in the table.
+
+
+
+
+    }
+}
